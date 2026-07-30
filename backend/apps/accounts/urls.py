2046a1597877447eda2d2649714ecd8a4e04c123
Rupesh Kumar Mahto <rupeshit06531@gmail.com>
2026-07-30 15:58:53 +0530

    add accounts urls

---
+++ backend/apps/accounts/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+
+from .views import (
+    RegisterAPIView,
+    LoginAPIView,
+    CurrentUserAPIView,
+)
+
+
+urlpatterns = [
+    path(
+        "register/",
+        RegisterAPIView.as_view(),
+        name="register"
+    ),
+
+    path(
+        "login/",
+        LoginAPIView.as_view(),
+        name="login"
+    ),
+
+    path(
+        "me/",
+        CurrentUserAPIView.as_view(),
+        name="current_user"
+    ),
+]
