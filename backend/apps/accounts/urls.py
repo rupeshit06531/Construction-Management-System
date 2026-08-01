034e6e474bae037bedb0e4c7989ebfb7c11401ea
Rupesh Kumar Mahto <rupeshit06531@gmail.com>
2026-08-01 12:32:15 +0530

    Add password change API endpoint

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -4,6 +4,7 @@ from .views import (
     RegisterAPIView,
     LoginAPIView,
     CurrentUserAPIView,
+    PasswordChangeAPIView,
 )
 
 
@@ -11,18 +12,24 @@ urlpatterns = [
     path(
         "register/",
         RegisterAPIView.as_view(),
-        name="register"
+        name="register",
     ),
 
     path(
         "login/",
         LoginAPIView.as_view(),
-        name="login"
+        name="login",
     ),
 
     path(
         "me/",
         CurrentUserAPIView.as_view(),
-        name="current_user"
+        name="current_user",
+    ),
+
+    path(
+        "password/change/",
+        PasswordChangeAPIView.as_view(),
+        name="password_change",
     ),
 ]
