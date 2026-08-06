c8b4f7a625a90677f066bc3bdd0f74e4c7cda358
Rupesh Kumar Mahto <rupeshit06531@gmail.com>
2026-08-06 16:04:14 +0530

    Apply role based permissions to projects API

--- backend/apps/projects/views.py
+++ backend/apps/projects/views.py
@@ -1,5 +1,10 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+
+from apps.common.permissions import (
+    IsManagerOrAbove,
+    IsStaffUser,
+    IsSuperAdmin,
+)
 
 from .models import Project
 from .serializers import ProjectSerializer
@@ -8,13 +13,41 @@ from .serializers import ProjectSerializer
 class ProjectListCreateAPIView(generics.ListCreateAPIView):
     queryset = Project.objects.select_related("manager").all()
     serializer_class = ProjectSerializer
-    permission_classes = [IsAuthenticated]
 
-    def perform_create(self, serializer):
-        serializer.save()
+    def get_permissions(self):
+        if self.request.method == "GET":
+            permission_classes = [IsStaffUser]
+
+        elif self.request.method == "POST":
+            permission_classes = [IsManagerOrAbove]
+
+        else:
+            permission_classes = [IsManagerOrAbove]
+
+        return [
+            permission()
+            for permission in permission_classes
+        ]
 
 
 class ProjectDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Project.objects.select_related("manager").all()
     serializer_class = ProjectSerializer
-    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            permission_classes = [IsStaffUser]
+
+        elif self.request.method in ["PUT", "PATCH"]:
+            permission_classes = [IsManagerOrAbove]
+
+        elif self.request.method == "DELETE":
+            permission_classes = [IsSuperAdmin]
+
+        else:
+            permission_classes = [IsStaffUser]
+
+        return [
+            permission()
+            for permission in permission_classes
+        ]
